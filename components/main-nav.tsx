1af4d40979c89baa339db51763aaccdce34d88ee
Mustafa Basel <mbasel@bimser.com.tr>
2024-02-28 23:57:34 +0300

    Main-nav route bug fixed

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,17 +12,17 @@ export function MainNav({
   const params = useParams()
   const routes = [
     {
-      href: `${params.storeId}`,
+      href: `/${params.storeId}`,
       label: "Overview",
       active: pathName === `/${params.storeId}`
     },
     {
-      href: `${params.storeId}/billboards`,
+      href: `/${params.storeId}/billboards`,
       label: "Billboards",
       active: pathName === `/${params.storeId}/billboards`
     },
     {
-      href: `${params.storeId}/settings`,
+      href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathName === `/${params.storeId}/settings`
     }
