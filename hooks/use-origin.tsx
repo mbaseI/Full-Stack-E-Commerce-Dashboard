4063de9589d361eb32f231eb80f6e3984b5ed58e
Mustafa Basel <83069151+mbaseI@users.noreply.github.com>
2024-03-20 06:54:18 +0300

    Origin Bug Test ( Vercel )

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -2,11 +2,8 @@ import { useEffect, useState } from "react"
 
 export const useOrigin = () => {
   const [mounted, setMounted] = useState(false)
-  const origin =
-    typeof window !== "undefined" && window.location.origin
-      ? window.location.origin
-      : ""
-
+  const origin = "https://full-stack-e-commerce-dashboard.vercel.app/"
+   
   useEffect(() => {
     setMounted(true)
   }, [])
