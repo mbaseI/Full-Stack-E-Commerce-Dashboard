c52767d4a799f70bb974675a4fec077f62691048
Mustafa Basel <83069151+mbaseI@users.noreply.github.com>
2024-03-20 17:21:46 +0300

    Update prismadb.ts

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,10 +1,15 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-    var prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+    return new PrismaClient()
 }
 
-const prismadb = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
+declare global {
+    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export default prismadb;
+const prismadb = globalThis.prisma ?? prismaClientSingleton()
+
+export default prismadb
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb
